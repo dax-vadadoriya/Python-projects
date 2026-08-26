509f401ecec2c2b7567451d424555697f5069607
dax-vadadoriya <dvadadoriya8@gmail.com>
2026-08-26 16:52:16 +0530

    Implement loop for multiple guesses in number guessing game
    Refactor the guessing logic to allow multiple attempts and count guesses.

--- Number guessing game.py
+++ Number guessing game.py
@@ -2,11 +2,17 @@ import random
 
 secret_number = random.randint(1, 10)
 
-guess = int(input("Guess the number: "))
+guess = 0
+attempts = 0
 
-if guess > secret_number:
-    print("Too high")
-elif guess < secret_number:
-    print("Too low")
-else:
-    print("Correct!")
+while guess != secret_number:
+    guess = int(input("Guess the number: "))
+    attempts = attempts + 1
+
+    if guess > secret_number:
+        print("Too high")
+    elif guess < secret_number:
+        print("Too low")
+    else:
+        print("Correct!")
+        print("You got it in", attempts, "guesses.")
